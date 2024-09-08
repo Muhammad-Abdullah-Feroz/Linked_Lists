f789e0272215ecb96c0034893d699b0c9822c8ed
abdullahferoz313@gmail.com <abdullahferoz313@gmail.com>
2024-09-08 15:53:04 +0500

    Created a self referential class for NODE

--- linked_list_with_classes.cpp
+++ linked_list_with_classes.cpp
@@ -2,3 +2,15 @@
 #include<conio.h>
 
 using namespace std;
+
+class node{
+    int value;
+    node * next;
+public:
+    void next(node * newNode){
+        this->next = newNode;
+    }
+    int value(){
+        return this->value;
+    }
+};
