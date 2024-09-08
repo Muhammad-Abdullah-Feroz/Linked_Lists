a34e588b35d87e79f4f370b0b06485de5bc3f329
abdullahferoz313@gmail.com <abdullahferoz313@gmail.com>
2024-09-08 16:27:18 +0500

    1- Edited the Setters and Getters for the node class. 2- Added the append function to List class

--- linked_list_with_classes.cpp
+++ linked_list_with_classes.cpp
@@ -1,16 +1,61 @@
-#include<iostream>
-#include<conio.h>
+#include <iostream>
+#include <conio.h>
 
 using namespace std;
 
-class node{
+class node
+{
     int value;
-    node * next;
+    node *next;
+
 public:
-    void next(node * newNode){
+    void setNext(node *newNode)
+    {
         this->next = newNode;
     }
-    int value(){
+    void setValue(int value)
+    {
+        this->value = value;
+    }
+    node *getNext()
+    {
+        return next;
+    }
+    int getValue()
+    {
         return this->value;
+    }
+};
+
+class integerList
+{
+private:
+    node *head;
+
+public:
+    integerList()
+    {
+        this->head = NULL;
+    }
+
+    void append(int value)
+    {
+        node *newNode = new node;
+
+        newNode->setValue(value);
+        newNode->setNext(nullptr);
+
+        if(!head){
+            head = newNode;
+        }else{
+            node * nodePtr = head;
+            node* prev;
+
+            while(nodePtr->getNext() != nullptr){
+                nodePtr = nodePtr->getNext();
+            }
+
+            nodePtr->setNext(newNode);
+        }
     }
 };
