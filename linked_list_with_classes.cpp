539c7b05a8a4808c54c2f558f8afc7ff4ca6e19c
abdullahferoz313@gmail.com <abdullahferoz313@gmail.com>
2024-09-10 13:48:12 +0500

    Added deleteNode function

--- linked_list_with_classes.cpp
+++ linked_list_with_classes.cpp
@@ -38,17 +38,23 @@ public:
         this->head = NULL;
     }
 
-    void display(){
-        if(!head){
-            cout<<endl<<"The list is empty..."<<endl;
-        }else{
-            node * nodePtr = head;
+    void display()
+    {
+        if (!head)
+        {
+            cout << endl
+                 << "The list is empty..." << endl;
+        }
+        else
+        {
+            node *nodePtr = head;
 
-            while(nodePtr){
-                cout<<nodePtr->getValue()<<" . ";
+            while (nodePtr)
+            {
+                cout << nodePtr->getValue() << " . ";
                 nodePtr = nodePtr->getNext();
             }
-            cout<<endl;
+            cout << endl;
         }
     }
 
@@ -59,32 +65,42 @@ public:
         newNode->setValue(value);
         newNode->setNext(nullptr);
 
-        if(!head){
+        if (!head)
+        {
             head = newNode;
-        }else{
-            node * nodePtr = head;
-            node* prev;
+        }
+        else
+        {
+            node *nodePtr = head;
+            node *prev;
 
-            while(nodePtr->getNext() != nullptr){
+            while (nodePtr->getNext() != nullptr)
+            {
                 nodePtr = nodePtr->getNext();
             }
 
             nodePtr->setNext(newNode);
         }
     }
-    
-    void insert(int index , int value){
-        if (index < 0){
-            cout<<endl<<"Invalid index...";
-        }
-        else if(index == 0)
+
+    void insert(int index, int value)
+    {
+        if (index < 0)
         {
-            if(!head){
+            cout << endl
+                 << "Invalid index...";
+        }
+        else if (index == 0)
+        {
+            if (!head)
+            {
                 head = new node;
                 head->setValue(value);
                 head->setNext(nullptr);
-            }else{
-                node* newNode = new node;
+            }
+            else
+            {
+                node *newNode = new node;
                 newNode->setValue(value);
                 newNode->setNext(head);
                 head = newNode;
@@ -92,14 +108,16 @@ public:
         }
         else
         {
-            node * nodePtr = head;
-            node * newNode = new node;
-            node * prev;
+            node *nodePtr = head;
+            node *newNode = new node;
+            node *prev;
 
             newNode->setValue(value);
 
-            while(nodePtr){
-                if (index == 0){
+            while (nodePtr)
+            {
+                if (index == 0)
+                {
                     break;
                 }
                 index--;
@@ -107,43 +125,80 @@ public:
                 nodePtr = nodePtr->getNext();
             }
 
-            if(index == 0){
+            if (index == 0)
+            {
                 newNode->setNext(nodePtr);
                 prev->setNext(newNode);
-            }else{
-                cout<<endl<<"Invalid Index...";
+            }
+            else
+            {
+                cout << endl
+                     << "Invalid Index...";
             }
         }
     }
 
-    void insertAtBegin(int value){
-        insert(0,value);
+    void insertAtBegin(int value)
+    {
+        insert(0, value);
     }
 
-    void insertAtEnd(int value){
+    void insertAtEnd(int value)
+    {
         append(value);
     }
 
+    void deleteNode(int value)
+    {
+        node *nodePtr = head;
+        node *prev = nullptr;
+
+        while (nodePtr->getNext())
+        {
+            if (nodePtr->getValue() == value)
+            {
+                break;
+            }
+
+            prev = nodePtr;
+            nodePtr = nodePtr->getNext();
+        }
+
+        if (nodePtr->getValue() == value)
+        {
+            if (head == nodePtr)
+            {
+                head = nodePtr->getNext();
+            }
+            else
+            {
+                prev->setNext(nodePtr->getNext());
+                // delete nodePtr
+            }
+            delete nodePtr;
+        }
+    }
 };
 
-
-int main(){
+int main()
+{
     integerList list;
     list.display();
     list.append(2);
     list.append(5);
     list.append(7);
     list.display();
-    list.insert(4,8);
+    list.insert(3, 8);
     // list.insert(9);
     list.display();
     // list.delete(2);
     list.display();
-    // list.delete(5);
+    list.deleteNode(5);
     list.display();
-    // list.delete(9);
+    list.deleteNode(2);
     list.display();
-    // list.delete(11);
+    list.insertAtEnd(11);
+    list.insertAtBegin(11);
     list.display();
     _getch();
     return 0;
