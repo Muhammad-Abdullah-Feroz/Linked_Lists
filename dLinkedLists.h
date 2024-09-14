8d267f5c1ccc793163c005d14443634fff6d442f
abdullahferoz313@gmail.com <abdullahferoz313@gmail.com>
2024-09-14 14:45:43 +0500

    Creating Dual Linked Lists with extra optimization

---
+++ dLinkedLists.h
@@ -0,0 +1,44 @@
+#pragma once
+
+#include<iostream>
+#include<conio.h>
+
+using namespace std;
+
+class node
+{
+private:
+    /* data */
+public:
+    node();
+    node(int value);
+};
+
+node::node(/* args */)
+{
+}
+node::node(int value)
+{
+}
+
+node::~node()
+{
+}
+
+
+class dList
+{
+private:
+    /* data */
+public:
+    dList();
+    ~dList();
+};
+
+dList::dList()
+{
+}
+
+dList::~dList()
+{
+}
