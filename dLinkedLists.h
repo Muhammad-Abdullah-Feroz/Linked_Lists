11754a4bb4d9299a9c39d224adb996e6f32feb13
abdullahferoz313@gmail.com <abdullahferoz313@gmail.com>
2024-09-14 15:29:51 +0500

    Completed th enode class and added the append function to dList class

--- dLinkedLists.h
+++ dLinkedLists.h
@@ -8,36 +8,88 @@ using namespace std;
 class node
 {
 private:
-    /* data */
+    int value;
+    node* next;
+    node* prev;
 public:
     node();
     node(int value);
+    int getValue();
+    node* getNext();
+    node* getPrev();
+    void setValue(int newVal);
+    void setNext(node * nextNode);
+    void setPrev(node * prevNode);
 };
 
-node::node(/* args */)
+node::node()
 {
-}
-node::node(int value)
-{
+    value = 0;
+    next = nullptr;
+    prev = nullptr;
 }
 
-node::~node()
+node::node(int value)
 {
+    this->value = value;
+    next = nullptr;
+    prev = nullptr;
+}
+
+int node::getValue(){
+    return this->value;
+}
+node* node::getNext(){
+    return this->next;
+}
+node* node::getPrev(){
+    return this->prev;
+}
+
+void node::setNext(node * nextNode){
+    this->next = nextNode;
+}
+void node::setPrev(node * prevNode){
+    this->prev = prevNode;
+}
+void node::setValue(int newVal){
+    this->value = newVal;
 }
 
 
 class dList
 {
 private:
-    /* data */
+    node * head;
+    node * tail;
 public:
     dList();
     ~dList();
+    void append(int value);
+
 };
 
 dList::dList()
 {
+    head = nullptr;
+    tail = nullptr;
 }
+
+void dList::append(int value){
+    node * newNode = new node(value);
+
+    if(head == nullptr){
+        head = newNode;
+        tail = head;
+    }else{
+        // nodePtr = tail;
+        tail->setNext(newNode);
+        newNode->setPrev(tail);
+        tail = newNode;
+    }
+
+}
+
 
 dList::~dList()
 {
