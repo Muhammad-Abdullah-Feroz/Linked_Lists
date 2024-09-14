1cd4a545dfe4086e63775d2aa344146cf6482677
abdullahferoz313@gmail.com <abdullahferoz313@gmail.com>
2024-09-14 15:32:22 +0500

    created a driver to test dList class

---
+++ dList_driver.cpp
@@ -0,0 +1,39 @@
+#include "dLinkedLists.h"
+
+int main()
+{
+    dList list;
+    // list.display();
+    list.append(5);
+    list.append(2);
+    list.append(7);
+    list.append(6);
+    list.append(3);
+    // list.display();
+    // list.insert(3, 8);
+    // // list.insert(9);
+    // list.display();
+    // // list.delete(2);
+    // list.display();
+    // list.deleteNode(5);
+    // list.display();
+    // list.deleteNode(2);
+    // list.display();
+    // list.insertAtEnd(11);
+    // list.insertAtBegin(11);
+    // list.display();
+    // list.update(9, 3);
+    // list.display();
+
+    // list.search(5);
+    // list.search(11);
+    // list.search(8);
+    // list.sort();
+    // list.display();
+    // list.reverse();
+    // list.display();
+
+
+    _getch();
+    return 0;
+}
