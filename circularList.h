8697446f8f93855893d03ce2adb596abffb413e7
abdullahferoz313@gmail.com <abdullahferoz313@gmail.com>
2024-09-20 17:17:24 +0500

    Added the basic insertion and display functions to the cList class

--- circularList.h
+++ circularList.h
@@ -15,26 +15,102 @@ public:
         this->value = val;
         this->next = nullptr;
     }
-    void setValue(int val){
+    void setValue(int val)
+    {
         this->value = val;
     }
-    int getValue(){
+    int getValue()
+    {
         return this->value;
     }
-    void setNext(node * nextNode){
+    void setNext(node *nextNode)
+    {
         this->next = nextNode;
     }
-    node * getNext(){
+    node *getNext()
+    {
         return this->next;
     }
 };
 
-class cList{
-    node* head;
-    node* tail;
-    public:
-        cList(){
-            head = nullptr;
-            tail = nullptr;
+class cList
+{
+    node *head;
+    node *tail;
+
+public:
+    cList()
+    {
+        head = nullptr;
+        tail = nullptr;
+    }
+    void display(){
+        node * nodePtr = head;
+        cout<<endl;
+        for(int i = 0 ; i < 10 ; i++){
+            cout<<nodePtr->getValue()<<" -> ";
+            nodePtr = nodePtr->getNext();
         }
+    }
+    void insertAtStart(int value)
+    {
+        node *newNode = new node(value);
+        newNode->setNext(head);
+        if (head == nullptr)
+        {
+            tail = newNode;
+            head = newNode;
+        }
+        else
+        {
+            head = newNode;
+        }
+
+        tail->setNext(head);
+    }
+    void append(int value)
+    {
+        node *newNode = new node(value);
+        newNode->setNext(head);
+        node *nodePtr = head;
+        if (head)
+        {
+            while (nodePtr != tail)
+            {
+                nodePtr = nodePtr->getNext();
+            }
+            nodePtr->setNext(newNode);
+            tail = newNode;
+        }
+        else
+            insertAtStart(value);
+    }
+    void insertAtIndex(int idx , int value){
+        if(idx < 0){
+            cout<<endl<<"Invalid Index...";
+        }else{
+            if (idx == 0){
+                insertAtStart(value);
+                return;
+            }
+            node * nodePtr = head;
+            node * previous = nullptr;
+            while(previous != tail && idx != 0){
+                previous = nodePtr;
+                nodePtr = nodePtr->getNext();
+                idx--;
+            }
+
+            if(idx == 0){
+                node* newNode = new node(value);
+                previous->setNext(newNode);
+                newNode->setNext(nodePtr);
+                if(previous == tail){
+                    tail = newNode;
+                }
+            }else{
+                cout<<endl<<"Invalid Index...";
+            }
+        }
+    }
 };
